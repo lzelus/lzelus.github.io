fde4549feac69069dc4e8f91b04eccac57f00790
lzelus <lzelus@users.noreply.github.com>
2023-08-21 17:35:06 -0700

    Add PKCE Support

--- APIM/config.js
+++ APIM/config.js
@@ -2,14 +2,14 @@ var Config = function(defaults, localStorageId) {
     if (!localStorage) {
         console.log("Warning: LocalStorage is not supported, config will not auto save");
     }
-    
+
     this._defaults = defaults;
     this._localStorageId = localStorageId;
     _.defaults(this, {
         _defaults: {},
         _localStorageId:"config"
     })
-    
+
     // Normalize everything to the object form
     this._defaults = _.mapObject(this._defaults, function(v,k) {
         if (_.has(v, "value")) {
@@ -18,22 +18,22 @@ var Config = function(defaults, localStorageId) {
             return {value:v};
         }
     })
-    
-    _.extend(this, 
+
+    _.extend(this,
         _.mapObject(this._defaults, function(v,k) {
             return v.value;
         })
     );
-    
+
     this._load(_.keys(defaults));
-    
+
     console.log("Loaded.", this);
 }
 
 Config.prototype._load = function(keys) {
     if (!localStorage) {return;}
     var loaded = JSON.parse(localStorage.getItem(this._localStorageId) || "{}");
-    
+
     if (keys.length) {
         loaded = _.pick(loaded, keys);
     }
@@ -53,16 +53,16 @@ Config.prototype._keys = function() {
 Config.prototype._buildTable = function(table) {
     _.each(this._defaults, function (settings, id) {
         var input = $("<input>", {"id":id});
-        
+
         if (this[id]) input.val(this[id])
-    
+
         $(table).append(
             $("<tr>").append(
                 $("<th>").text(id),
                 $("<td>").append(input)
             )
         );
-        
+
         var config = this;
 
         input.change(function (e) {
@@ -77,20 +77,21 @@ Config.prototype._buildTable = function(table) {
 }
 
 Config.prototype._buildForm = function(form) {
-    var template = $(form).find("div.template").remove();
-    template.removeClass("template");
-    
+    var template = $(form).find(".row_template").remove();
+    template.removeClass("row_template");
+
     _.each(this._defaults, function (settings, id) {
         var field = template.clone();
         form.append(field);
-        
+
         var input = field.find("input");
-        
+
         input.attr("id", id);
         field.find("label").attr("for", id).text(id);
-        
+
+        // Set the value of the input to the current config value
         if (this[id]) input.val(this[id])
-    
+
         var config = this;
 
         input.change(function (e) {
