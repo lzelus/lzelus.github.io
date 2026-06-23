3a86cfecd8a828207cc11c58477ce2612119d568
lzelus <lzelus@ucsd.edu>
2026-06-23 12:08:30 -0700

    Add profiles and make PKCE a checkbox

--- APIM/config.js
+++ APIM/config.js
@@ -1,105 +1,126 @@
-var Config = function(defaults, localStorageId) {
+var Config = function(defaults) {
     if (!localStorage) {
         console.log("Warning: LocalStorage is not supported, config will not auto save");
     }
 
     this._defaults = defaults;
-    this._localStorageId = localStorageId;
-    _.defaults(this, {
-        _defaults: {},
-        _localStorageId:"config"
-    })
 
-    // Normalize everything to the object form
-    this._defaults = _.mapObject(this._defaults, function(v,k) {
-        if (_.has(v, "value")) {
-            return v;
-        } else {
-            return {value:v};
-        }
-    })
+    // Normalize all defaults to {value: ...} form
+    this._defaults = _.mapObject(this._defaults, function(v, k) {
+        return _.has(v, "value") ? v : {value: v};
+    });
 
-    _.extend(this,
-        _.mapObject(this._defaults, function(v,k) {
-            return v.value;
-        })
-    );
+    // Start from defaults, then load any saved values for the active profile
+    _.extend(this, _.mapObject(this._defaults, function(v) { return v.value; }));
+    this._load();
 
-    this._load(_.keys(defaults));
-
-    console.log("Loaded.", this);
+    console.log("Loaded config for profile '" + this._activeProfile() + "'", this);
 }
 
-Config.prototype._load = function(keys) {
-    if (!localStorage) {return;}
-    var loaded = JSON.parse(localStorage.getItem(this._localStorageId) || "{}");
+// --- Profile helpers ---
 
-    if (keys.length) {
-        loaded = _.pick(loaded, keys);
-    }
-    _.extend(this, loaded);
+Config.prototype._activeProfile = function() {
+    return localStorage ? (localStorage.getItem("activeProfile") || "default") : "default";
+}
+
+Config.prototype._storageKey = function() {
+    return "config_" + this._activeProfile();
+}
+
+Config.prototype.getProfiles = function() {
+    if (!localStorage) { return ["default"]; }
+    return JSON.parse(localStorage.getItem("profileList") || '["default"]');
+}
+
+Config.prototype._saveProfileList = function(profiles) {
+    if (localStorage) { localStorage.setItem("profileList", JSON.stringify(profiles)); }
+}
+
+// --- Core load/save ---
+
+Config.prototype._load = function() {
+    if (!localStorage) { return; }
+    var saved = JSON.parse(localStorage.getItem(this._storageKey()) || "{}");
+    // Only restore keys that exist in defaults
+    _.extend(this, _.pick(saved, _.keys(this._defaults)));
 }
 
 Config.prototype._save = function() {
-    if (!localStorage) {return;}
-    var data = _.pick(this, this._keys());
-    localStorage.setItem(this._localStorageId, JSON.stringify(data));
+    if (!localStorage) { return; }
+    var data = _.pick(this, _.keys(this._defaults));
+    localStorage.setItem(this._storageKey(), JSON.stringify(data));
 }
 
-Config.prototype._keys = function() {
-    return _.filter(_.keys(this), function(k) {return k[0] != "_"})
-}
+// --- Profile switching ---
 
-Config.prototype._buildTable = function(table) {
-    _.each(this._defaults, function (settings, id) {
-        var input = $("<input>", {"id":id});
+// Save current values, then load the named profile's values and refresh form inputs.
+Config.prototype.switchProfile = function(name) {
+    this._save();
 
-        if (this[id]) input.val(this[id])
+    localStorage.setItem("activeProfile", name);
 
-        $(table).append(
-            $("<tr>").append(
-                $("<th>").text(id),
-                $("<td>").append(input)
-            )
-        );
+    // Reset to defaults then overlay saved values for the new profile
+    _.extend(this, _.mapObject(this._defaults, function(v) { return v.value; }));
+    this._load();
 
-        var config = this;
-
-        input.change(function (e) {
-            console.log("In input change");
-            if (settings.onChange) settings.onChange(e)
-            console.log("Setting " + e.target.id + " to " + e.target.value);
-            config[e.target.id] = e.target.value;
-            config._save();
-        });
-        settings.input = input;
+    // Refresh rendered form inputs
+    _.each(this._defaults, function(settings, id) {
+        if (settings.input) {
+            settings.input.val(this[id] != null ? this[id] : "");
+        }
     }, this);
 }
+
+// Create a new profile, copying current values into it, and switch to it.
+Config.prototype.createProfile = function(name) {
+    var profiles = this.getProfiles();
+    if (!_.contains(profiles, name)) {
+        profiles.push(name);
+        this._saveProfileList(profiles);
+    }
+    localStorage.setItem("activeProfile", name);
+    this._save();  // saves current values under the new profile key
+}
+
+// Delete a profile's saved data. Caller is responsible for switching away first.
+Config.prototype.deleteProfile = function(name) {
+    var profiles = _.without(this.getProfiles(), name);
+    this._saveProfileList(profiles);
+    if (localStorage) { localStorage.removeItem("config_" + name); }
+}
+
+// --- Form builder ---
 
 Config.prototype._buildForm = function(form) {
     var template = $(form).find(".row_template").remove();
     template.removeClass("row_template");
 
-    _.each(this._defaults, function (settings, id) {
+    _.each(this._defaults, function(settings, id) {
         var field = template.clone();
         form.append(field);
 
         var input = field.find("input");
-
         input.attr("id", id);
         field.find("label").attr("for", id).text(id);
 
-        // Set the value of the input to the current config value
-        if (this[id]) input.val(this[id])
-
         var config = this;
 
-        input.change(function (e) {
-            console.log("Setting " + e.target.id + " to " + e.target.value);
-            if (settings.onChange) settings.onChange(e)
-            config[e.target.id] = e.target.value;
-            config._save();
-        });
+        if (settings.type !== "checkbox") {
+            input.addClass("form-control");
+            if (this[id]) { input.val(this[id]); }
+            input.change(function(e) {
+                config[e.target.id] = e.target.value;
+                config._save();
+            });
+        } else {
+            input.attr("type", "checkbox").addClass("form-check-input");
+            input.prop("checked", !!this[id]);
+            input.change(function(e) {
+                config[e.target.id] = e.target.checked;
+                config._save();
+            });
+        }
+
         settings.input = input;
     }, this);
 }
